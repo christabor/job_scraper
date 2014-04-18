e055367c493ce8a46ab9bc9386203fe0706f3403
christobot <dxdstudio@gmail.com>
2014-04-17 21:21:39 -0700

    Add some more options, decompose functions further

--- run.py
+++ run.py
@@ -3,33 +3,37 @@ import os
 from pyquery import PyQuery as pq
 
 hrefs = {}
+ALL_CATEGORIES = 'all-job-categories.json'
 
 
 def process_all():
     with open('index.html', 'rb') as htmlfile:
         html = pq(htmlfile.read())
         html.find('a').each(_process_link)
-    f = open('all-job-categories.json', 'wb')
+    f = open(ALL_CATEGORIES, 'wb')
     f.write(json.dumps(hrefs))
     f.write('\n')
     f.close()
 
 
+def process_one(keyword):
+    cmd = ('scrapy crawl careerbuilder-job '
+           '-a category={category} -o '
+           'data/{category}.json -t json'.format(category=keyword))
+    try:
+        os.system(cmd)
+    except OSError:
+        pass
+
+
 def run_all():
     """Use with caution"""
-    with open('all-job-categories.json', 'rb') as jobs:
+    with open(ALL_CATEGORIES, 'rb') as jobs:
         categories = dict(json.loads(jobs.read()))
         jobs.close()
         for k, keywords in categories.iteritems():
             for keyword in keywords:
-                cmd = ('scrapy crawl careerbuilder-job '
-                       '-a category={category} -o '
-                       'data/{category}.json -t json'.format(category=keyword))
-                print cmd
-                try:
-                    os.system(cmd)
-                except OSError:
-                    continue
+                process_one(keyword)
 
 
 def _process_link(k, link):
@@ -43,7 +47,14 @@ def _process_link(k, link):
     except KeyError:
         hrefs[letter] = []
 
-process_first = raw_input('Process json first? YES/NO ==> ')
-if process_first == 'YES':
+process_first = raw_input('Process json first? y/n ==> ')
+run_all_tests = raw_input('Run all tests? y/n ==> ')
+
+if process_first == 'y':
     process_all()
-run_all()
+
+if run_all_tests == 'y':
+    run_all()
+else:
+    category = raw_input('Enter a category to run: ')
+    process_one(category)
