41f6a6108245ecddc8a04296cb0f6b5fa46dad45
christobot <dxdstudio@gmail.com>
2014-04-17 22:39:38 -0700

    Add mongo settings

--- jobs/settings.py
+++ jobs/settings.py
@@ -9,9 +9,9 @@
 BOT_NAME = 'jobs'
 SPIDER_MODULES = ['jobs.spiders']
 NEWSPIDER_MODULE = 'jobs.spiders'
-ITEM_PIPELINES = {
-    'jobs.pipelines.FilterJobDetailsPipeline': 0,
-}
+ITEM_PIPELINES = [
+    'jobs.pipelines.FilterJobDetailsPipeline',
+]
 
 # Crawl responsibly by identifying yourself
 # (and your website) on the user-agent
@@ -20,3 +20,14 @@ USER_AGENT = 'jobs scraping tool'
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
 DOWNLOAD_DELAY = 0.2
+
+# MongoDB pipeline
+MONGODB_URI = 'mongodb://localhost:27017'
+MONGODB_DATABASE = 'jobs'
+MONGODB_COLLECTION = 'jobs'
+MONGODB_ADD_TIMESTAMP = True
+
+USE_DB = False
+
+if USE_DB:
+    ITEM_PIPELINES.append('scrapy_mongodb.MongoDBPipeline')
