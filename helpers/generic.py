8f16a920760df535fb6657ad3e0b08849b8cbc77
Chris Tabor <dxdstudio@gmail>
2015-08-15 19:55:05 -0700

    Update all helpers

--- helpers/generic.py
+++ helpers/generic.py
@@ -2,7 +2,7 @@ import os
 
 
 def get_spiders_list():
-    # Convention: 'name' attribute used for scrapy should match
+    # Convention: 'name' class attribute used for scrapy should match
     # the file name, minus the _spider string.
     files = ['{}'.format(name.replace('_spider.py', '')) for name in
              os.listdir('jobs/spiders') if name.endswith('py')
--- helpers/onetonline.py
+++ helpers/onetonline.py
@@ -1,10 +1,9 @@
-import json
-import os
-from pyquery import PyQuery as Pq
-
 hrefs = {}
 ALL_CATEGORIES = 'fixtures/onet_all-job-categories.json'
 
 
 class OnetOnlineHelper:
-    pass
+
+    @staticmethod
+    def load_onet_categories():
+        raise NotImplemented
