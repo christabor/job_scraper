f8b4d7680438a2e0bcac6c5e92b886e70d326d9e
Chris Tabor <dxdstudio@gmail>
2015-08-15 19:54:35 -0700

    More fixes to run after refactor

--- run.py
+++ run.py
@@ -3,24 +3,27 @@ from helpers import generic as gen
 from helpers.careerbuilder import CareerBuilderHelper as Cb
 from helpers.onetonline import OnetOnlineHelper as Onet
 
-process_first = raw_input('Process json first? y/n ==> ')
-run_all_tests = raw_input('Run all tests? y/n ==> ')
-
 print('Enter a spider to run: ')
 print(gen.get_spiders_list())
 spider = raw_input('Spider: ==> ')
 
 if spider == 'careerbuilder':
+    process_first = raw_input('Process json first? y/n ==> ')
+    run_all_tests = raw_input('Run all tests? y/n ==> ')
     if process_first == 'y':
         Cb.process_all()
     if run_all_tests == 'y':
         Cb.run_all()
     else:
         print('Enter a job title to run:')
-        ppr(Cb.CareerBuilderHelper.load_categories())
+        ppr(Cb.load_categories())
         keyword = raw_input('Title: ==> ')
         if keyword:
             Cb.process_one(spider, keyword)
-elif spider == 'onet':
-    print('Enter a job category to run:')
+
+elif spider == 'onet_categories':
+    print('Not implemented...')
     ppr(Onet.load_onet_categories())
+
+elif spider == 'onet_jobs':
+    print('Not implemented...')
